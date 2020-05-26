6eff6b1a876bc77a8adce09d0e1cd6163d731297
FDolotov <59660051+FDolotov@users.noreply.github.com>
2020-05-27 00:26:22 +0300

    Add files via upload

---
+++ Lab1_main.py
@@ -0,0 +1,138 @@
+import time
+import pylab
+import matplotlib.pyplot as plt
+from SortsL1 import bubble_sort, quick_sort ,merge_sort
+from DBgenL1Backup import generate, teacher, structure_scheme, structure_data
+
+"""
+Основная функция лабораторной
+param db_filename: имя базы данных
+type: db_filename: str
+param: count_range: массив кол - ва обьектов в базе данных
+type: count_range: iterable 
+param: structure_shceme: структура данных для генерации
+type: structure_scheme: tuple(tuple(str, iterable) .. str)
+param: structure_data: данные для генерации структуры
+type: structure_data: dict('key' : [val])
+"""
+
+def make(db_file_name, count_range, structure_scheme, structure_data):
+   results = {'quick': [], 'bubble': [], 'merge':[]}
+   for count in count_range:
+       print('{} elements '.format(count))
+       #Генерируем базу данных
+       generate(db_file_name, count, structure_scheme, structure_data) 
+       
+       #Извлекаем данные из файла в массив
+       file = open(db_file_name, 'r')
+       tch_list = []
+       for line in file:
+          tch_list.append(teacher(*line.replace('\n', '').split(';')))
+       
+       print('bubble sort start')
+       tch_list_sorted = tch_list.copy()
+       tb = time.time() # запускаем счетчик времени
+       bubble_sort(tch_list_sorted) # выполняем сортировку пузырьком
+       ta = time.time()
+       results['bubble'].append((count, ta - tb))
+       
+       del tch_list_sorted
+       
+       print('quick sort start')
+       tch_list_sorted = tch_list.copy()
+       tb = time.time() # запускаем счетчик времени
+       quick_sort(tch_list_sorted) # выполняем быструю сортировку
+       ta = time.time()
+       results['quick'].append((count, ta - tb))
+       
+       del tch_list_sorted
+       
+       print('merge sort start')
+       tch_list_sorted = tch_list.copy()
+       tb = time.time() # запускаем счетчик времени
+       merge_sort(tch_list_sorted) # выполняем сортировку слиянием
+       ta = time.time()
+       results['merge'].append((count, ta - tb))
+       print('\n')
+       
+       del tch_list_sorted
+       
+       del tch_list 
+
+   #Построение графиков
+   xlist = count_range
+   ylist1 = [el[1] for el in results['bubble']]
+   ylist2 = [el[1] for el in results['quick']]
+   ylist3 = [el[1] for el in results['merge']]
+   plt.loglog(xlist, ylist1, label = 'Сортировка пузырьком')
+   plt.loglog(xlist, ylist2, label = 'Быстрая')
+   plt.loglog(xlist, ylist3, label = 'Сортировка слиянием')
+   pylab.title('Сравнение быстрой, пузырьковой и сортировки слиянием')
+   pylab.xlabel('Количество объектов')
+   pylab.ylabel('Время на сортировку')
+   pylab.legend(title='Легенда')
+   
+   #Вывод количества элементов, и времени сортировки
+   print("Bubble")
+   for element in results['bubble']:
+       print(str(element[0]) + ';' + str(element[1]))
+   print('\n')
+
+       
+   print("Quick")
+   for element in results['quick']:
+       print(str(element[0]) + ';' + str(element[1]))
+   print('\n')
+   
+   print("Merge")
+   for element in results['merge']:
+       print(str(element[0]) + ';' + str(element[1]))
+   print('\n')
+       
+   pylab.show()
+   
+"""
+Функция сортировки пузырьком
+param file_name: имя базы данных
+type: file_name: str
+return: sorted list
+"""
+
+def print_bubble_sort(file_name):
+    file = open(file_name, 'r')
+    tch_list = []
+    for line in file:
+        tch_list.append(teacher(*line.replace('\n', '').split(';')))
+    print(bubble_sort(tch_list))
+
+"""
+Функция быстрой сортировки
+param: file_name: имя базы данных
+type: file_name: str
+return: sorted list
+"""
+ 
+def print_quick_sort(file_name):
+    file = open(file_name, 'r')
+    tch_list = []
+    for line in file:
+        tch_list.append(teacher(*line.replace('\n', '').split(';')))
+    print(quick_sort(tch_list))
+
+"""
+Функция сортировки слиянием
+param: file_name: имя базы данных
+type: file_name: str
+return: sorted list
+"""
+  
+def print_merge_sort(file_name):
+    file = open(file_name, 'r')
+    tch_list = []
+    for line in file:
+        tch_list.append(teacher(*line.replace('\n', '').split(';')))
+    print(merge_sort(tch_list))
+    
+elements_quantity = [1000, 1500, 2000, 3000, 4000]
+filename = 'teachers.txt'
+make(filename, elements_quantity, structure_scheme, structure_data)
---
+++ SortsL1.py
@@ -0,0 +1,87 @@
+import random
+import operator
+
+"""
+Функция сортировки пузырьком
+param: array: массив, который должен быть отсортирован
+type: array:  iterative with random access and comparable elements
+return: sorted list
+"""
+
+def bubble_sort(array):
+    N = len(array)
+    for i in range(N-1):
+        for j in range(N-i-1):
+            if array[j] > array[j+1]:
+                buff = array[j]
+                array[j] = array[j+1]
+                array[j+1] = buff
+    return array
+
+"""
+Функция быстрой сортировки
+param: array: массив, который должен быть отсортирован
+type: array: iterative with random access and comparable elements
+return: sorted list
+"""
+            
+def quick_sort(array):
+   if len(array) < 2:
+       return array
+   else:
+       q = random.choice(array)
+       s_elem = []
+       m_elem = []
+       e_elem = []
+       for n in array:
+           if n < q:
+               s_elem.append(n)
+           elif n > q:
+               m_elem.append(n)
+           else:
+               e_elem.append(n)
+       return quick_sort(s_elem) + e_elem + quick_sort(m_elem)
+   
+"""
+Функция сортировки слиянием
+param: file_name: массив, который должен быть отсортирован
+type: file_name:  iterative with random access and comparable elements
+return: sorted list
+"""
+
+def merge_sort(array, compare=operator.lt): #operator.lt - <
+    if len(array) < 2:
+        return array
+    else:
+        middle = int(len(array) / 2)
+        #Дробление входного массива на половины
+        left = merge_sort(array[:middle], compare)
+        right = merge_sort(array[middle:], compare)
+        return merge(left, right, compare)
+    
+"""
+Функция, сравнивающая элементы из левой и правой части
+param: left: левая граница
+type: left: iterable
+param: right: правая граница
+type: right: iterable
+return: list
+"""
+
+def merge(left, right, compare):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if compare(left[i], right[j]):
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
